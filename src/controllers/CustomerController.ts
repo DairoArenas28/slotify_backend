802f8a1549141063da260500de65d0111bb67d44
DAIRO ARENAS <dairo@sistemasia.com>
2025-08-05 22:44:36 -0500

    se implementa el enpoint de la paginacion en customer

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -8,6 +8,35 @@ export class CustomerController {
         res.status(201).json(customers)
     }
 
+    static getPaginationAll = async (req: Request, res: Response) => {
+        try {
+            const limit = parseInt(req.query.limit as string) || 10
+            const page = parseInt(req.query.page as string) || 1
+            const offset = (page - 1) * limit
+
+            /*let whereOptions = undefined
+            if(req.user.role === "client"){
+                whereOptions = { userId: req.user.id };
+            }*/
+            const user = await Customer.findAndCountAll({
+                order: [
+                    ['createdAt', 'DESC']
+                ],
+                //where: whereOptions,
+                limit,
+                offset
+            })
+            res.json({
+                total: user.count,
+                page,
+                totalPages: Math.ceil(user.count / limit),
+                data: user.rows 
+            })
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+    }
+
     static create = async (req: Request, res: Response) => {
         try {
             console.log(req.body)
